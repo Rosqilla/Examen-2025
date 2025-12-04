981bfec3a20fa5f05965339cc8651e49d80ed76b
José Manuel Vidal Sánchez <jmvidal2002@gmail.com>
2025-12-04 19:59:08 +0100

    Archivos sin importancia

--- app/src/main/java/com/example/entrenamiento/OnEntrenamientoAddedListener.java
+++
@@ -1,6 +0,0 @@
-package com.example.entrenamiento;
-
-public interface OnEntrenamientoAddedListener {
-    void onEntrenamientoAdded(String nombre, String descripcion, String duracion, String dificultad);
-}
-
